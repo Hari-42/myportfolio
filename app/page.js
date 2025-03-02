c6912861bc384a8ce966ad8dfdcddf43297c274d
Hari-42 <harisaranmohanathas@hotmail.com>
2025-03-02 23:31:26 +0100

    added footer

--- app/page.js
+++ app/page.js
@@ -2,7 +2,8 @@
 
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans">
+        <div
+            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans">
             <main className="text-center">
                 <h1 className="text-[12rem] font-bold text-black leading-none">
                     HARI
@@ -11,6 +12,59 @@ export default function Home() {
                     SOFTWARE / GAME DEVELOPER
                 </h2>
             </main>
+
+            <footer>
+                <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+                    <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8"/>
+                    <div className="md:flex md:justify-between">
+                        <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-4 w-full">
+                            <div className="mb-6 md:mb-0">
+                                <a className="flex items-center">
+                                    <span
+                                        className="self-center text-2xl font-bold whitespace-nowrap text-black">HARI</span>
+                                </a>
+                            </div>
+                            <div className="mb-6 md:mb-0">
+                                <h2 className="mb-6 text-2xl font-bold text-black">Legal matters</h2>
+                                <ul className="text-gray-800 font-medium">
+                                    <li>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="mb-6 md:mb-0">
+                                <h2 className="mb-6 text-2xl font-bold text-black">Resources</h2>
+                                <ul className="text-gray-800 font-medium">
+                                    <li className="mb-4">
+                                        <a href="https://tailwindcss.com/"
+                                           className="hover:text-blue-500 transition-colors duration-300">Tailwind
+                                            CSS</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="mb-6 md:mb-0">
+                                <h2 className="mb-6 text-2xl font-bold text-black">Socials</h2>
+                                <ul className="text-gray-800 font-medium">
+                                    <li className="mb-4">
+                                        <a href='https://discord.gg/mjNcZFHnf5'
+                                           className="hover:text-blue-500 transition-colors duration-300">Discord -
+                                            Community Server</a>
+                                    </li>
+                                    <li className="mb-4">
+                                        <a href='https://github.com/Hari-42'
+                                           className="hover:text-blue-500 transition-colors duration-300">Github -
+                                            Hari-42</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8"/>
+                    <div className="flex items-center justify-center flex-wrap">
+                        <span
+                            className="text-sm text-black-500 text-center">© 2025 MKH-GAMES All Rights Reserved.</span>
+                    </div>
+                </div>
+            </footer>
         </div>
     );
 }
