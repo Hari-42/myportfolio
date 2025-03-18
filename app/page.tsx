5cf08932c78feb1355f4d68e10b47f19425497e5
Hari-42 <harisaranmohanathas@hotmail.com>
2025-03-18 12:52:16 +0100

    routing config

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
+import { FC } from 'react';
 
-
-export default function Home() {
+const Home: FC = () => {
     return (
         <div
             className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans">
@@ -18,8 +18,8 @@ export default function Home() {
             </main>
 
             <footer>
-            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-                    <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8"/>
+                <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+                    <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8" />
                     <div className="md:flex md:justify-between">
                         <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-4 w-full">
                             <div className="mb-6 md:mb-0">
@@ -38,7 +38,7 @@ export default function Home() {
                                 <h2 className="mb-6 text-2xl font-bold text-black">Resources</h2>
                                 <ul className="text-gray-800 font-medium">
                                     <li className="mb-4">
-                                    <a href="https://tailwindcss.com/"
+                                        <a href="https://tailwindcss.com/"
                                            className="hover:text-blue-500 transition-colors duration-300">Tailwind
                                             CSS</a>
                                     </li>
@@ -66,7 +66,7 @@ export default function Home() {
                             </div>
                         </div>
                     </div>
-                    <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8"/>
+                    <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8" />
                     <div className="flex items-center justify-center flex-wrap">
                         <span
                             className="text-sm text-black-500 text-center">© 2025 MKH-GAMES All Rights Reserved.</span>
@@ -75,5 +75,6 @@ export default function Home() {
             </footer>
         </div>
     );
-}
+};
 
+export default Home;
